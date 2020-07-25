fc3e296fe51498a4056b9302b9e531be72201ccb
atakan99 <49478791+atakan99@users.noreply.github.com>
2020-07-25 11:48:01 +0300

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('postman-request');
+
+const forecast = (latitude, longitude, callback)=>{
+    const url = 'http://api.weatherstack.com/current?access_key=5fee3f988f0fd330a4eb68c858245851&query=' + latitude + ',' + longitude +'&units=m';
+
+    //shorthand for url => url: url => url
+    request({url, json: true}, (error, {body})=>{
+        if (error){
+            callback('Unable to connect to weather service', undefined)
+        }else if (body.error){
+             callback('Unable the find location', undefined);   
+        }else{
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out and it fells like ' + body.current.feelslike + ' degrees out.')
+        }
+
+
+    })
+}
+
+module.exports = forecast;
